23779883c9dfc5070ca56183741a851003e14f21
mohammedRasif <mdrasif3499@gmail.com>
2025-04-16 22:02:31 +0600

    create

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Shared/Banner";
 import Opportunities from "../Shared/Opportunities";
+import ServiceAndBusiness from "../Shared/ServiceAndBusiness";
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <Opportunities/>
+            <ServiceAndBusiness/>
         </div>
     );
 }
